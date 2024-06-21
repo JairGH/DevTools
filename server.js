d426356d21500ba1b1a2f1f488b2ef02433febcf
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-06-21 16:41:29 -0700

    figuring out the layout

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,8 +10,26 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+function renderWithLayout(res, view, options = {}) {
+  res.render(view, (err, html) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.render("layout", {
+        title: options.title || "DevTools",
+        cssFile: options.cssFile || null,
+        body: html,
+      });
+    }
+  });
+}
+
 app.get("/", (req, res) => {
-  res.render("index");
+  renderWithLayout(res, "index");
+});
+
+app.get("/monitors", (req, res) => {
+  res.render("monitors");
 });
 
 const PORT = process.env.PORT || 3000;
