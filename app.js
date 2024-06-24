9f90ab59b2ed0944158ee52f36078dbbb05b6727
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-06-23 21:26:15 -0700

    Switched to handlebars, changed folder structure, everything is working as before

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { create } = require("express-handlebars");
+
+const hbs = create({
+  defaultLayout: "main",
+  extname: ".handlebars",
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/monitors", (req, res) => {
+  res.render("monitors");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
