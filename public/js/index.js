2842bc00a4c42ed98f90cf8c3e360a7262d88bdc
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-06-27 20:22:14 -0700

    Added href to display divs on main page with js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 function redirectToMonitors() {
   window.location.href = "http://localhost:3000/monitors";
 }
-
 document.getElementById("monitorClick").onclick = redirectToMonitors;
+
+function redirectToKeyboards() {
+  window.location.href = "http://localhost:3000/keyboards";
+}
+document.getElementById("keyboardClick").onclick = redirectToKeyboards;
+
+function redirectToMouse() {
+  window.location.href = "http://localhost:3000/mouse";
+}
+document.getElementById("mouseClick").onclick = redirectToMouse;
+
+function redirectToHeadphones() {
+  window.location.href = "http://localhost:3000/headphones";
+}
+document.getElementById("headphonesClick").onclick = redirectToHeadphones;
+
+function redirectToCommunity() {
+  window.location.href = "http://localhost:3000/community";
+}
+document.getElementById("communityClick").onclick = redirectToCommunity;
