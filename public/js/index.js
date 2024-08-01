38bd779693463a8093bcb7f28935a039ead885eb
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-07-31 21:44:55 -0700

    Creating a better structure on community page.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const { Carousel } = require("bootstrap");
+
 // For mobile
 function showSidebar() {
   const sidebar = document.getElementById("sidebar");
@@ -12,48 +14,43 @@ document.addEventListener("DOMContentLoaded", () => {
   const sideMenuBtn = document.getElementById("sideMenuBtn");
   if (sideMenuBtn) {
     sideMenuBtn.addEventListener("click", showSidebar);
+  } else {
+    console.log("Something went wrong!");
   }
 });
 // Create Account Form
-let accountFormBtn = document.getElementById("create-account-btn");
-let loginFormBtn = document.getElementById("login-btn");
-let createAccountForm = document.getElementById("create-account");
-let loginForm = document.getElementById("login-form");
-let communityMessage = document.getElementById("message-container");
-
-accountFormBtn.addEventListener("click", () => {
-  communityMessage.style.display = "none";
-  createAccountForm.style.display = "flex";
+let createAccountBtn = document.getElementById("create-account-btn");
+createAccountBtn.addEventListener("click", () => {
+  const accountForm = document.getElementById("create-account-form");
+  const messageForm = document.getElementById("firstOptionDiv");
+  accountForm.style.display = "flex";
 });
 
-loginFormBtn.addEventListener("click", () => {
-  communityMessage.style.display = "none";
-  loginForm.style.display = "flex";
-});
-
-// Main page div
-function setDiv(div, page) {
-  document.getElementById(div).onclick = () => redirectToPage(page);
-}
 const allLinks = {
   monitorLink: "http://localhost:3000/monitors",
-  monitorDiv: "monitorClick",
   keyboardLink: "http://localhost:3000/keyboards",
-  keyboardDiv: "keyboardClick",
   mouseLink: "http://localhost:3000/mouse",
-  mouseDiv: "mouseClick",
   headphonesLink: "http://localhost:3000/headphones",
-  headphonesDiv: "headphonesClick",
   communityLink: "http://localhost:3000/community",
-  communityDiv: "communityClick",
 };
 
-function redirectToPage(page) {
-  window.location.href = page;
-}
-
-setDiv(allLinks.monitorDiv, allLinks.monitorLink);
-setDiv(allLinks.keyboardDiv, allLinks.keyboardLink);
-setDiv(allLinks.mouseDiv, allLinks.mouseLink);
-setDiv(allLinks.headphonesDiv, allLinks.headphonesLink);
-setDiv(allLinks.communityDiv, allLinks.communityLink);
+let monitorDiv = document.getElementById("monitorClick");
+monitorDiv.addEventListener("click", () => {
+  window.location.href = allLinks.monitorLink;
+});
+let keyboardDiv = document.getElementById("keyboardClick");
+keyboardDiv.addEventListener("click", () => {
+  window.location.href = allLinks.keyboardLink;
+});
+let mouseDiv = document.getElementById("mouseClick");
+mouseDiv.addEventListener("click", () => {
+  window.location.href = allLinks.mouseLink;
+});
+let headphonesDiv = document.getElementById("headphonesClick");
+headphonesDiv.addEventListener("click", () => {
+  window.location.href = allLinks.headphonesLink;
+});
+let communityDiv = document.getElementById("communityClick");
+communityDiv.addEventListener("click", () => {
+  window.location.href = allLinks.communityLink;
+});
