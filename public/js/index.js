3d75f97e3171969e3d978c02bcb19b982da0c4e9
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-08-01 17:04:55 -0700

    Create account form UI is done (maybe I will chnage some CSS), fixing my JS

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,22 @@
-const { Carousel } = require("bootstrap");
+// function showSidebar() {
+//   const sidebar = document.getElementById("sidebar");
+//   if (sidebar.style.display === "" || sidebar.style.display === "none") {
+//     sidebar.style.display = "flex";
+//   } else {
+//     sidebar.style.display = "none";
+//   }
+// }
 
-// For mobile
-function showSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.display === "" || sidebar.style.display === "none") {
-    sidebar.style.display = "flex";
-  } else {
-    sidebar.style.display = "none";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const sideMenuBtn = document.getElementById("sideMenuBtn");
-  if (sideMenuBtn) {
-    sideMenuBtn.addEventListener("click", showSidebar);
-  } else {
-    console.log("Something went wrong!");
-  }
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const sideMenuBtn = document.getElementById("sideMenuBtn");
+//   if (sideMenuBtn) {
+//     sideMenuBtn.addEventListener("click", showSidebar);
+//   } else {
+//     console.log("Something went wrong!");
+//   }
+// });
 // Create Account Form
-let createAccountBtn = document.getElementById("create-account-btn");
-createAccountBtn.addEventListener("click", () => {
-  const accountForm = document.getElementById("create-account-form");
-  const messageForm = document.getElementById("firstOptionDiv");
-  accountForm.style.display = "flex";
-});
-
+console.log(window.location);
 const allLinks = {
   monitorLink: "http://localhost:3000/monitors",
   keyboardLink: "http://localhost:3000/keyboards",
@@ -35,22 +26,40 @@ const allLinks = {
 };
 
 let monitorDiv = document.getElementById("monitorClick");
-monitorDiv.addEventListener("click", () => {
-  window.location.href = allLinks.monitorLink;
-});
+if (monitorDiv) {
+  monitorDiv.addEventListener("click", () => {
+    window.location.href = allLinks.monitorLink;
+  });
+}
 let keyboardDiv = document.getElementById("keyboardClick");
-keyboardDiv.addEventListener("click", () => {
-  window.location.href = allLinks.keyboardLink;
-});
+if (keyboardDiv) {
+  keyboardDiv.addEventListener("click", () => {
+    window.location.href = allLinks.keyboardLink;
+  });
+}
 let mouseDiv = document.getElementById("mouseClick");
-mouseDiv.addEventListener("click", () => {
-  window.location.href = allLinks.mouseLink;
-});
+if (mouseDiv) {
+  mouseDiv.addEventListener("click", () => {
+    window.location.href = allLinks.mouseLink;
+  });
+}
 let headphonesDiv = document.getElementById("headphonesClick");
-headphonesDiv.addEventListener("click", () => {
-  window.location.href = allLinks.headphonesLink;
-});
+if (headphonesDiv) {
+  headphonesDiv.addEventListener("click", () => {
+    window.location.href = allLinks.headphonesLink;
+  });
+}
 let communityDiv = document.getElementById("communityClick");
-communityDiv.addEventListener("click", () => {
-  window.location.href = allLinks.communityLink;
-});
+if (communityDiv) {
+  communityDiv.addEventListener("click", () => {
+    window.location.href = allLinks.communityLink;
+  });
+}
+
+// let createAccountBtn = document.getElementById("create-account-btn");
+// createAccountBtn.addEventListener("click", () => {
+//   const accountForm = document.getElementById("account-card-form");
+//   const messageForm = document.getElementById("message-card-form");
+//   messageForm.style.display = "none";
+//   accountForm.style.display = "flex";
+// });
