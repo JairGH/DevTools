3fcc26629c0feb3ce141c22fc58229409a21795b
Jair Gomez <jairgomez@Jairs-Laptop.local>
2024-08-16 21:56:22 -0700

    Is working for both plattaforms.

--- public/js/index.js
+++ public/js/index.js
@@ -99,20 +99,23 @@ if (loginBtn) {
 }
 // Create post form
 let createPostBtn = document.getElementById("create-btn");
-let imageSkeleton = document.getElementsByClassName("skeleton-placeholder");
+let imageSkeleton = document.getElementsByClassName("skeleton-placeholder")[0];
 const showSkeleton = () => {
-  imageSkeleton.style.visibility = "visible";
+  imageSkeleton.style.display = "block";
 };
 if (createPostBtn) {
   let userPhoto = document.getElementById("user-photo");
   let inputFile = document.getElementById("input-file");
-  createPostBtn.addEventListener("click", () => {
+  let imageTag = document.getElementById("user-photo");
+  imageTag.style.display = createPostBtn.addEventListener("click", () => {
     let createPostForm = document.getElementById("post-form");
     createPostForm.style.visibility = "visible";
     showSkeleton();
     createPostBtn.style.visibility = "hidden";
     inputFile.onchange = () => {
       userPhoto.src = URL.createObjectURL(inputFile.files[0]);
+      imageSkeleton.style.display = "none";
+      userPhoto.style.display = "block";
     };
   });
 }
